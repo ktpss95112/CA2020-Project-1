6b21d76f4bdfedecffdb79ba6d527bbe19ad99e6
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2020-12-04 15:03:04 +0800

    initial commit

---
+++ gen.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+TARGET=GodKaienLinAndHisWorshippers_project1
+TMPFOLDER=/tmp/$RANDOM/$TARGET
+
+if [[ -f ${TARGET}.zip ]]; then
+    rm -f ${TARGET}.zip
+fi
+
+mkdir -p $TMPFOLDER/codes
+
+cp codes/*.v $TMPFOLDER/codes
+cp report.pdf $TMPFOLDER/${TARGET}.pdf
+
+rm $TMPFOLDER/code/Instruction_Memory.v
+rm $TMPFOLDER/code/Data_Memory.v
+rm $TMPFOLDER/code/PC.v
+rm $TMPFOLDER/code/Registers.v
+rm $TMPFOLDER/code/testbench.v
+
+zip -r ${TARGET}.zip $TMPFOLDER
+
+rm -rf $TMPFOLDER
