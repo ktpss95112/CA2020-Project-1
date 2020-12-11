fc9bf133f71f283891c1ca1707eacb2b427cfc3b
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2020-12-11 15:59:04 +0800

    Finish hazard detection part, waiting for forwarding part to debug

--- codes/Control.v
+++ codes/Control.v
@@ -4,6 +4,7 @@
 `include "const.v"
 
 module Control (
+    NoOp_i,
     Op_i,
     RegWrite_o,
     MemtoReg_o,
@@ -14,6 +15,7 @@ module Control (
     Branch_o
 );
 
+input           NoOp_i;
 input   [6:0]   Op_i;
 
 output reg          RegWrite_o;
@@ -25,53 +27,70 @@ output reg          ALUSrc_o;
 output reg          Branch_o;
 
 always @(Op_i) begin
-    case (Op_i)
-        `OPCODE_R_TYPE: begin
-            RegWrite_o  <= 1'b1;
-            MemtoReg_o  <= 1'b0;
-            MemRead_o   <= 1'b0;
-            MemWrite_o  <= 1'b0;
-            ALUOp_o     <= 2'b01;
-            ALUSrc_o    <= 1'b0;
-        end
+    if (NoOp_i) begin
+        RegWrite_o  <= 1'b0;
+        MemtoReg_o  <= 1'b0;
+        MemRead_o   <= 1'b0;
+        MemWrite_o  <= 1'b0;
+        ALUOp_o     <= 2'b0;
+        ALUSrc_o    <= 1'b0;
+        Branch_o    <= 1'b0;
+    end
 
-        `OPCODE_I_TYPE: begin
-            RegWrite_o  <= 1'b1;
-            MemtoReg_o  <= 1'b0;
-            MemRead_o   <= 1'b0;
-            MemWrite_o  <= 1'b0;
-            ALUOp_o     <= 2'b00;
-            ALUSrc_o    <= 1'b1;
-        end
+    else begin
+        case (Op_i)
+            `OPCODE_R_TYPE: begin
+                RegWrite_o  <= 1'b1;
+                MemtoReg_o  <= 1'b0;
+                MemRead_o   <= 1'b0;
+                MemWrite_o  <= 1'b0;
+                ALUOp_o     <= 2'b01;
+                ALUSrc_o    <= 1'b0;
+                Branch_o    <= 1'b0;
+            end
 
-        `OPCODE_LOAD: begin
-            RegWrite_o  <= 1'b1;
-            MemtoReg_o  <= 1'b1;
-            MemRead_o   <= 1'b1;
-            MemWrite_o  <= 1'b0;
-            ALUOp_o     <= 2'b00;
-            ALUSrc_o    <= 1'b1;
-        end
+            `OPCODE_I_TYPE: begin
+                RegWrite_o  <= 1'b1;
+                MemtoReg_o  <= 1'b0;
+                MemRead_o   <= 1'b0;
+                MemWrite_o  <= 1'b0;
+                ALUOp_o     <= 2'b00;
+                ALUSrc_o    <= 1'b1;
+                Branch_o    <= 1'b0;
+            end
 
-        `OPCODE_STORE: begin
-            RegWrite_o  <= 1'b0;
-            MemtoReg_o  <= 1'b0;
-            MemRead_o   <= 1'b0;
-            MemWrite_o  <= 1'b1;
-            ALUOp_o     <= 2'b01;
-            ALUSrc_o    <= 1'b1;
-        end
+            `OPCODE_LOAD: begin
+                RegWrite_o  <= 1'b1;
+                MemtoReg_o  <= 1'b1;
+                MemRead_o   <= 1'b1;
+                MemWrite_o  <= 1'b0;
+                ALUOp_o     <= 2'b00;
+                ALUSrc_o    <= 1'b1;
+                Branch_o    <= 1'b0;
+            end
 
-        `OPCODE_BRANCH: begin
-            RegWrite_o  <= 1'b0;
-            MemtoReg_o  <= 1'b0;
-            MemRead_o   <= 1'b0;
-            MemWrite_o  <= 1'b0;
-            ALUOp_o     <= 2'b11;
-            ALUSrc_o    <= 1'b0;
-        end
+            `OPCODE_STORE: begin
+                RegWrite_o  <= 1'b0;
+                MemtoReg_o  <= 1'b0;
+                MemRead_o   <= 1'b0;
+                MemWrite_o  <= 1'b1;
+                ALUOp_o     <= 2'b01;
+                ALUSrc_o    <= 1'b1;
+                Branch_o    <= 1'b0;
+            end
 
-    endcase
+            `OPCODE_BRANCH: begin
+                RegWrite_o  <= 1'b0;
+                MemtoReg_o  <= 1'b0;
+                MemRead_o   <= 1'b0;
+                MemWrite_o  <= 1'b0;
+                ALUOp_o     <= 2'b11;
+                ALUSrc_o    <= 1'b0;
+                Branch_o    <= 1'b1;
+            end
+
+        endcase
+    end
 end
 
 endmodule
--- codes/Hazard_Detection.v
+++ codes/Hazard_Detection.v
@@ -2,10 +2,27 @@
 `define HAZARD_DETECTION_V
 
 module Hazard_Detection (
-    Stall_o
+    MemRead_i,
+    RS1addr_i,
+    RS2addr_i,
+    RDaddr_i,
+    Stall_o,
+    NoOp_o,
+    PCWrite_o
 );
 
-output  Stall_o;
+input           MemRead_i;
+input   [4:0]   RS1addr_i;
+input   [4:0]   RS2addr_i;
+input   [4:0]   RDaddr_i;
+
+output          Stall_o;
+output          NoOp_o;
+output          PCWrite_o;
+
+assign Stall_o = (MemRead_i && (RDaddr_i == RS1addr_i || RDaddr_i == RS2addr_i));
+assign NoOp_o = Stall_o;
+assign PCWrite_o = ~Stall_o;
 
 endmodule
 
